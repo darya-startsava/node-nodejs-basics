808ada61888ab0229b130fbc3b9c14306ccbcf1f
darya-startsava <daryastartsava@gmail.com>
2023-06-14 22:00:21 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt');
+  if (!existsSync(fileToReadPath)) {
+    throw new Error('FS operation failed');
+  } else {
+    const content = await readFile(fileToReadPath, { encoding: 'utf8' });
+    console.log(content);
+  }
 };
 
 await read();
