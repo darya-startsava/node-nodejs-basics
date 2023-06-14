89e8d16a7abf2aa13bdc02cc4419b63cb9bf56e3
darya-startsava <daryastartsava@gmail.com>
2023-06-14 14:13:42 +0200

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const fileToRemovePath = join(__dirname, 'files', 'fileToRemove.txt');
+  if (!existsSync(fileToRemovePath)) {
+    throw new Error('FS operation failed');
+  } else { rm(fileToRemovePath); }
 };
 
 await remove();
