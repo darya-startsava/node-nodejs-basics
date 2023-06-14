70058972f3f65ee46b59ec36c928bae44b33cded
darya-startsava <daryastartsava@gmail.com>
2023-06-14 13:08:49 +0200

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fsPromises from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const beforeRenameFilePath = join(__dirname, 'files', 'wrongFilename.txt');
+  const afterRenameFilePath = join(__dirname, 'files', 'properFilename.md');
+  if (existsSync(afterRenameFilePath) || !existsSync(beforeRenameFilePath)) {
+    throw new Error('FS operation failed');
+  } else { fsPromises.rename(beforeRenameFilePath, afterRenameFilePath); }
 };
 
 await rename();
