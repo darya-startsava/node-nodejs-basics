8e07aa6bf45e6c8da9cf62481af96c36daae5ef6
darya-startsava <daryastartsava@gmail.com>
2023-06-14 21:45:07 +0200

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filesFolderPath = join(__dirname, 'files');
+  if (!existsSync(filesFolderPath)) {
+    throw new Error('FS operation failed');
+  } else {
+    const files = await readdir(filesFolderPath);
+    console.log(files);
+  }
 };
 
 await list();
