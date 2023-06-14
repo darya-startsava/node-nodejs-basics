12d69c503b2c58c48f0051d74fa01073f450a691
darya-startsava <daryastartsava@gmail.com>
2023-06-14 02:15:38 +0200

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const newFilePath = join(__dirname, 'files', 'fresh.txt');
+  if (existsSync(newFilePath)) {
+    throw new Error('FS operation failed');
+  } else {
+    writeFile(newFilePath, 'I am fresh and young');
+  }
 };
 
 await create();
