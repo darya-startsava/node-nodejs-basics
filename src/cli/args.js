dee3ca01ab42d62392340f3e832448122599664f
darya-startsava <daryastartsava@gmail.com>
2023-06-17 14:50:47 +0200

    feat: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  for (let i = 0; i < args.length; i++) {
+    if (/^--/.test(args[i])) {
+      console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+      i++;
+    }
+  }
 };
 
 parseArgs();
