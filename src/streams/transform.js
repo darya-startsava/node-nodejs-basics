e7f68393baa2721c8a9e0402c494a9c582ea46b5
darya-startsava <daryastartsava@gmail.com>
2023-06-18 16:00:55 +0200

    feat: implement transform stream function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseInput = (input) => input.split('').reverse().join('');
+
+  const transformInput = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, reverseInput(chunk.toString()));
+    }
+  });
+
+  await pipeline(process.stdin, transformInput, process.stdout);
+
 };
 
 await transform();
