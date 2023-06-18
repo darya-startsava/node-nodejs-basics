3dc67e40a6e2a14166641f9e2f060ce6a764906d
darya-startsava <daryastartsava@gmail.com>
2023-06-18 12:27:38 +0200

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const fileToCalculateHashForPath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const { createHash } = await import('node:crypto');
+  const hash = createHash('sha256');
+  const input = createReadStream(fileToCalculateHashForPath);
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
