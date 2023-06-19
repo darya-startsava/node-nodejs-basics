5d7a3830da1726be05f3b27d3815765431cb87be
darya-startsava <daryastartsava@gmail.com>
2023-06-19 02:37:09 +0200

    feat: spawns child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork } from 'node:child_process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const childProcessFilePath = join(__dirname, 'files', 'script.js');
+  const child = fork(childProcessFilePath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 11]);
